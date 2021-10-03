e15150e0fb89cd4d0e09921acae8c532c982687c
LeontiiMaksimov <60267651+LeontiiMaksimov@users.noreply.github.com>
2021-10-03 16:22:51 +0200

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -4,10 +4,10 @@
 
 void solver()
 {
-	double pres, temp, vol, mol;
-	std::cin >> pres >> vol >> temp;
-	mol = (pres * vol) / (R_Const * temp);
-	std::cout << mol << std::endl;
+	double pres, temp, vol, mol, mass;
+	std::cin >> mass >> pres >> vol >> temp;
+	mol = mass / (pres * vol) / (R_Const * temp);
+	std::cout << "The answer is:" << " " << mol << std::endl;
 }
 
 
@@ -16,7 +16,7 @@ int main()
 	int t;
 	std::cout << "Please input the amount of trials" << std::endl;
 	std::cin >> t;
-	std::cout << "For each trial please input the pressure, volume and temperature in this order separated by a space. Verify that the units are appropriate" << std::endl;
+	std::cout << "For each trial please input the mass, pressure, volume and temperature in this order separated by a space. Verify that the units are appropriate" << std::endl;
 	for (int i = 0; i < t; ++i)
 	{
 		solver();
